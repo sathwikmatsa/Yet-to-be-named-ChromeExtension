c863f6f0275d67160fbcc3874724d46241931fc2
Sathwik Matsa <sathwikmatsa@gmail.com>
2018-12-19 14:09:50 +0530

    add restart, pause and reset functionality to timer

--- extension/background/background.js
+++ extension/background/background.js
@@ -3,8 +3,11 @@ let countDownTimer;
 let interval;
 let countDownComplete;
 let alarm;
+let isPaused;
+let pausedTimeStamp;
 
 function timeIt(){
+    if(isPaused) return;
     if((millis() - startTime) > countDownTimer){
         countDownComplete = true;
         clearInterval(interval);
@@ -33,6 +36,7 @@ function setup(){
         countDownComplete = false;
         startTime = millis();
         console.log("init: ", startTime);
+        isPaused = false;
         interval = setInterval(timeIt, 1000);
     });
 }
@@ -40,10 +44,39 @@ function setup(){
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log("received request from popup!", countDownComplete, countDownTimer);
-    if (request.sendTimerDetails == true)
+    if (request.sendTimerDetails){
       sendResponse({
           current_time: millis() - startTime,
           countdown_time: countDownTimer,
           countdown_complete : countDownComplete
       });
+    }
+    else if(request.restartTimer){
+        countDownComplete = false;
+        if(isPaused){
+          startTime = millis() - (pausedTimeStamp - startTime);
+          isPaused = false;
+        }
+        else startTime = millis();
+        clearInterval(interval);
+        // TODO: make sure to update countDownTimer if changed
+        interval = setInterval(timeIt, 1000);
+        sendResponse({
+        current_time: millis() - startTime,
+        countdown_time: countDownTimer
+        });
+    }
+    else if(request.pauseTimer){
+        if(!countDownComplete){
+          pausedTimeStamp = millis();
+          isPaused = true;
+        }
+        sendResponse({is_paused: isPaused});
+    }
+    else if(request.resetTimer){
+        countDownComplete = true;
+        isPaused = false;
+        clearInterval(interval);
+        sendResponse({countdown_complete: true});
+    }
   });
--- extension/popup/sketch.js
+++ extension/popup/sketch.js
@@ -2,8 +2,36 @@ let startTime;
 let countDownTimer;
 let interval;
 let timer;
+let startButton;
+let pauseButton;
+let resetButton;
+let isPaused;
+
+function setup(){
+    noCanvas();
+    chrome.runtime.sendMessage({sendTimerDetails: true}, function(response) {
+        startTime = response.current_time;
+        countDownTimer = response.countdown_time;
+        let countDownComplete = response.countdown_complete;
+        timer = select("#timer");
+        if(!countDownComplete){
+            timeIt();
+            interval = setInterval(timeIt, 1000);
+        }
+        else timer.html(formattedTime(0));
+
+        startButton = select("#start");
+        startButton.mousePressed(restartTimer);
+        pauseButton = select("#pause");
+        pauseButton.mousePressed(pauseTimer);
+        resetButton = select("#reset");
+        resetButton.mousePressed(resetTimer);
+    });
+}
 
 function formattedTime(t){
+    // TODO: resolve this bug!
+    if(t > countDownTimer) t = countDownTimer;
     let time = floor(t/1000);
     let seconds = time % 60;
     time = floor(time/60);
@@ -14,24 +42,42 @@ function formattedTime(t){
 }
 
 function timeIt(){
+    if(isPaused) return;
     if((millis() + startTime) > countDownTimer){
+        timer.html(formattedTime(0));
         clearInterval(interval);
+        isPaused = true;
         return;
     }
     // 'countdown'
-    timer.html(formattedTime(countDownTimer - (millis() + startTime)));
+    timer.html(formattedTime(1000 + countDownTimer - (millis() + startTime)));
 }
 
-function setup(){
-    noCanvas();
-    chrome.runtime.sendMessage({sendTimerDetails: true}, function(response) {
-        startTime = response.current_time;
+function restartTimer(){
+    if(!isPaused) return;
+    chrome.runtime.sendMessage({restartTimer: true}, function(response) {
+        isPaused = false;
+        clearInterval(interval);
+        startTime = response.current_time - millis();
         countDownTimer = response.countdown_time;
-        let countDownComplete = response.countdown_complete;
-        timer = select("#timer");
-        if(!countDownComplete) interval = setInterval(timeIt, 1000);
-        else timer.html(formattedTime(0));
+        timeIt();
+        interval = setInterval(timeIt, 1000);
     });
 }
 
+function pauseTimer(){
+    if(isPaused) return;
+    chrome.runtime.sendMessage({pauseTimer: true}, function(response) {
+        isPaused = response.is_paused;
+    });
+}
 
+function resetTimer(){
+    console.log("reset pressed!");
+    chrome.runtime.sendMessage({resetTimer: true}, function(response) {
+        countDownComplete = response.countdown_complete;
+        clearInterval(interval);
+        timer.html(formattedTime(countDownTimer));
+        isPaused = true;
+    });
+}
