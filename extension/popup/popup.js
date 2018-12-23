b632d92550dffef0b03e9482841b5b4110dd6e35
Sathwik Matsa <sathwikmatsa@gmail.com>
2018-12-23 16:32:18 +0530

    setup persistent storage for todo-list

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,9 +1,16 @@
-document.getElementById("content").innerHTML='<object type="text/html" data="timer.html" width="730px" height="550px"></object>';
+let contentDiv = document.getElementById("content");
+contentDiv.innerHTML='<object type="text/html" data="timer.html" width="730px" height="550px"></object>';
+contentDiv.setAttribute("state", "timer");
 
 document.getElementById("timer").addEventListener("click", function(event){
-    document.getElementById("content").innerHTML='<object type="text/html" data="timer.html" width="730px" height="550px"></object>';
+    if(contentDiv.getAttribute("state") === "timer") return;
+    contentDiv.innerHTML='<object type="text/html" data="timer.html" width="730px" height="550px"></object>';
+    contentDiv.setAttribute("state", "timer");
 });
 
 document.getElementById("todo").addEventListener("click", function(event){
-    document.getElementById("content").innerHTML='<object type="text/html" data="todo.html" width="730px" height="550px"></object>';
+    if(contentDiv.getAttribute("state") === "todo") return;
+    contentDiv.innerHTML='<object type="text/html" data="todo.html" width="730px" height="550px"></object>';
+    contentDiv.setAttribute("state", "todo");
+
 });
--- extension/popup/todo.js
+++ extension/popup/todo.js
@@ -1,6 +1,25 @@
+let addTaskDiv = document.getElementById("add_task");
+let taskDiv = document.getElementById("tasks");
+let tasks;
+
+chrome.storage.sync.get(['NewTasks'], function(result) {
+    if(result.NewTasks){
+        console.log("tasks present!");
+        tasks = result.NewTasks;
+        let nTasks = tasks.length;
+        for(let i = 0; i < nTasks; i++){
+            let task = document.createElement("p");
+            task.className = "task";
+            task.innerText = tasks[i];
+            taskDiv.appendChild(task);
+        }
+    } else {
+        console.log("no tasks", result.key);
+        tasks = [];
+    }
+});
+
 document.getElementById("addTaskBtn").addEventListener("click", function(event){
-    let addTaskDiv = document.getElementById("add_task");
-    let taskDiv = document.getElementById("tasks");
     if(event.currentTarget.getAttribute("state") === "Normal"){
         addTaskDiv.innerHTML = `
             <input id="inputText" class="input" type="text" placeholder="enter your task">
@@ -10,8 +29,13 @@ document.getElementById("addTaskBtn").addEventListener("click", function(event){
         let input = document.getElementById("inputText");
         if(input.value === "") return;
         let task = document.createElement("p");
+        task.className = "task";
         task.innerText = input.value;
         taskDiv.appendChild(task);
+        tasks.push(input.value);
+        chrome.storage.sync.set({NewTasks: tasks}, function() {
+          console.log('New task added to storage :' + tasks[tasks.length-1]);
+        });
         event.currentTarget.setAttribute("state", "Normal");
         addTaskDiv.innerHTML = "";
     }
